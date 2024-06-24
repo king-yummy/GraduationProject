4e01e81f2b029f353a01f268fdfe26a48498f330
king-yummy <wingting2642@naver.com>
2024-06-24 15:27:14 +0900

    개폐업 분석 추가

--- client/src/components/ReportPage/index.jsx
+++ client/src/components/ReportPage/index.jsx
@@ -10,7 +10,10 @@ const ReportPage = () => {
   const { cityInfo, selectedDistrict, selectedIndustry } = location.state || {};
   const [chartData, setChartData] = useState(null);
   const [pieData, setPieData] = useState(null);
-  const [trendMessage, setTrendMessage] = useState("");
+  const [storeCountMessage, setStoreCountMessage] = useState("");
+  const [openChartData, setOpenChartData] = useState(null);
+  const [closeChartData, setCloseChartData] = useState(null);
+  const [changingCountMessage, setChangingCountMessage] = useState("");
 
   useEffect(() => {
     fetch("/assets/data/매출.csv")
@@ -34,23 +37,26 @@ const ReportPage = () => {
             }, {});
 
             const quarters = Object.keys(storeCounts).sort();
-            const counts = quarters.map(quarter => storeCounts[quarter]);
+            const counts = quarters.map((quarter) => storeCounts[quarter]);
 
             // 점포 수 변동 분석
-            let trendMessage = "점포 수 데이터를 분석할 수 없습니다.";
+            let storeCountMessage = "점포 수 데이터를 분석할 수 없습니다.";
             if (counts.length > 1) {
               const lastCount = counts[counts.length - 1];
               const previousCount = counts[counts.length - 2];
               if (lastCount > previousCount) {
-                trendMessage = "점포수가 전분기대비 증가하고 있습니다. 상권이 발달하는 시기인 경우 입지선정에 신중하셔야 합니다.";
+                storeCountMessage =
+                  "점포수가 전분기대비 증가하고 있습니다. 상권이 발달하는 시기인 경우 입지선정에 신중하셔야 합니다.";
               } else if (lastCount === previousCount) {
-                trendMessage = "점포수가 전분기대비 정체 상태입니다. 업소수 변동폭이 비교적 작습니다. 추가 개업시 경쟁이 심해지지 않을지 점검하세요.";
+                storeCountMessage =
+                  "점포수가 전분기대비 정체 상태입니다. 업소수 변동폭이 비교적 작습니다. 추가 개업시 경쟁이 심해지지 않을지 점검하세요.";
               } else {
-                trendMessage = "점포수가 전년동기에 비해 감소하고 있습니다. 상권이 쇠퇴하는 시기인 경우 창업에 유의하셔야 합니다.";
+                storeCountMessage =
+                  "점포수가 전년동기에 비해 감소하고 있습니다. 상권이 쇠퇴하는 시기인 경우 창업에 유의하셔야 합니다.";
               }
             }
 
-            setTrendMessage(trendMessage);
+            setStoreCountMessage(storeCountMessage);
 
             // 바 차트 데이터 구성
             const chartData = {
@@ -73,7 +79,9 @@ const ReportPage = () => {
 
             filteredData.forEach((row) => {
               franchiseCount += parseInt(row.프랜차이즈_점포_수, 10);
-              generalCount += parseInt(row.점포_수, 10) - parseInt(row.프랜차이즈_점포_수, 10);
+              generalCount +=
+                parseInt(row.점포_수, 10) -
+                parseInt(row.프랜차이즈_점포_수, 10);
             });
 
             // 파이차트 데이터 구성
@@ -86,8 +94,91 @@ const ReportPage = () => {
                 },
               ],
             };
-
             setPieData(pieData);
+
+            // 분기별 개업 및 폐업 데이터 계산
+            const openCounts = filteredData.reduce((acc, curr) => {
+              const quarter = curr.기준_년분기_코드;
+              acc[quarter] =
+                (acc[quarter] || 0) + parseInt(curr.개업_점포_수, 10);
+              return acc;
+            }, {});
+
+            const closeCounts = filteredData.reduce((acc, curr) => {
+              const quarter = curr.기준_년분기_코드;
+              acc[quarter] =
+                (acc[quarter] || 0) + parseInt(curr.폐업_점포_수, 10);
+              return acc;
+            }, {});
+
+            // 개업 차트 데이터 구성
+            const openChartData = {
+              labels: quarters,
+              datasets: [
+                {
+                  label: "개업 점포 수",
+                  data: quarters.map((quarter) => openCounts[quarter] || 0),
+                  backgroundColor: "rgba(54, 162, 235, 0.2)",
+                  borderColor: "rgba(54, 162, 235, 1)",
+                  borderWidth: 1,
+                },
+              ],
+            };
+            setOpenChartData(openChartData);
+
+            // 폐업 차트 데이터 구성
+            const closeChartData = {
+              labels: quarters,
+              datasets: [
+                {
+                  label: "폐업 점포 수",
+                  data: quarters.map((quarter) => closeCounts[quarter] || 0),
+                  backgroundColor: "rgba(255, 99, 132, 0.2)",
+                  borderColor: "rgba(255, 99, 132, 1)",
+                  borderWidth: 1,
+                },
+              ],
+            };
+            setCloseChartData(closeChartData);
+
+            // 개업 폐업수 변동 분석
+            let changingCountMessage = "점포 수 데이터를 분석할 수 없습니다.";
+            if (openCounts > 0) {
+              if (closeCounts > 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 증가, 폐업수는 증가하고 있습니다. 상권변화가 매우 활발하고 유동적입니다. 입지선정에 유의하세요.";
+              } else if (closeCounts < 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 증가, 폐업수는 감소하고 있습니다. 상권이 확장되고 있습니다. 경쟁관계 변화에 유의하세요.";
+              } else {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 증가, 폐업수는 동일합니다. 상권이 성장하는 추세입니다. 기회를 포착하고 시장 진입 전략을 세우세요.";
+              }
+            } else if (openCounts == 0) {
+              if (closeCounts > 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 동일, 폐업수는 증가하고 있습니다. 상권 내 경쟁이 심화되고 있습니다. 기존 업체와의 경쟁력 강화 방안을 마련하세요.";
+              } else if (closeCounts < 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 동일, 폐업수는 감소하고 있습니다. 상권 내 안정성이 높아지고 있습니다. 신규 진입에 유리한 시기를 활용하세요.";
+              } else {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수와 폐업수 모두 동일합니다. 상권의 안정성이 유지되고 있습니다. 안정적인 성장 기회를 찾고 꾸준한 운영을 목표로 하세요.";
+              }
+            } else {
+              if (closeCounts > 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 감소, 폐업수는 증가하고 있습니다. 상권이 위축되고 있습니다. 입지선정과 사업 리스크 관리에 신중을 기하세요.";
+              } else if (closeCounts < 0) {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수와 폐업수 모두 감소하고 있습니다. 상권의 활력이 줄어들고 있습니다. 비용 효율화와 시장 수요 변화에 적응할 준비를 하세요. ";
+              } else {
+                changingCountMessage =
+                  "전분기 대비 개업 업소수는 감소, 폐업수는 동일합니다. 상권이 안정기인것으로 보입니다. 마케팅활동에 유의하세요";
+              }
+            }
+
+            setChangingCountMessage(changingCountMessage);
           },
         });
       });
@@ -101,12 +192,41 @@ const ReportPage = () => {
         <p>선택된 업종: {selectedIndustry || "전체 업종"}</p>
       </Header>
       <Content>
-        <TrendMessage>{trendMessage}</TrendMessage>
+        <TrendMessage>{storeCountMessage}</TrendMessage>
         <ChartContainer>
-          {chartData ? <Bar data={chartData} options={{ maintainAspectRatio: false }} /> : <p>데이터를 불러오는 중...</p>}
+          {chartData ? (
+            <Bar data={chartData} options={{ maintainAspectRatio: false }} />
+          ) : (
+            <p>데이터를 불러오는 중...</p>
+          )}
         </ChartContainer>
         <ChartContainer>
-          {pieData ? <Pie data={pieData} options={{ maintainAspectRatio: false }} /> : <p>데이터를 불러오는 중...</p>}
+          {pieData ? (
+            <Pie data={pieData} options={{ maintainAspectRatio: false }} />
+          ) : (
+            <p>데이터를 불러오는 중...</p>
+          )}
+        </ChartContainer>
+        <TrendMessage>{changingCountMessage}</TrendMessage>
+        <ChartContainer>
+          {openChartData ? (
+            <Bar
+              data={openChartData}
+              options={{ maintainAspectRatio: false }}
+            />
+          ) : (
+            <p>데이터를 불러오는 중...</p>
+          )}
+        </ChartContainer>
+        <ChartContainer>
+          {closeChartData ? (
+            <Bar
+              data={closeChartData}
+              options={{ maintainAspectRatio: false }}
+            />
+          ) : (
+            <p>데이터를 불러오는 중...</p>
+          )}
         </ChartContainer>
       </Content>
     </ReportContainer>
@@ -134,7 +254,7 @@ const Content = styled.div`
 
 const ChartContainer = styled.div`
   width: 100%;
-  height: 400px; // 높이를 적절히 조정할 수 있습니다
+  height: 400px;
   margin-bottom: 20px;
 `;
 
